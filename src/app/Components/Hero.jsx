43715983bb43ca68d5c8c9a9715117cf56619582
shahrazmirza <shahrazahmedmirza@live.com>
2024-03-19 19:44:07 +1100

    Commit

--- src/app/Components/Hero.jsx
+++ src/app/Components/Hero.jsx
@@ -7,21 +7,6 @@ import 'animate.css';
 
 
 const Hero = () => {
-  const [currentImage, setCurrentImage] = useState('/assets/hero1.jpg');
-  const images = ['/assets/hero1.jpg', '/assets/hero2.jpg', '/assets/hero3.jpg', '/assets/hero4.jpg', '/assets/hero5.jpg']; 
-
-  const handleNextImage = () => {
-    const currentIndex = images.indexOf(currentImage);
-    const nextIndex = (currentIndex + 1) % images.length;
-    setCurrentImage(images[nextIndex]);
-  };
-
-  const handlePrevImage = () => {
-    const currentIndex = images.indexOf(currentImage);
-    const prevIndex = (currentIndex - 1 + images.length) % images.length;
-    setCurrentImage(images[prevIndex]);
-  };
-
   return (
       <div className='mh-screen'>
         <div className='absolute inset-0 overflow-hidden'>
@@ -31,25 +16,6 @@ const Hero = () => {
             layout='fill'
             objectFit='cover'
           />
-        </div>
-
-        <div className='flex justify-between items-center h-screen w-screen'>
-          <Link href='/' className='text-white text-6xl md:text-8xl font-light opacity-80' onClick={handlePrevImage}>
-            <div className='bg-black opacity-50 w-5 md:w-16 flex text-center items-center'>
-              <BsChevronLeft />
-            </div>
-          </Link>
-
-          <div className='text-center z-10'>
-            <p 
-            className='font-heading1 text-7xl md:text-8xl md: pb-5 tracking-wider text-orange-400 animate__animated animate__fadeInUp'>Sadaqa Aid</p>
-          </div>
-
-          <Link href='/' className='text-white text-6xl md:text-8xl font-light opacity-80' onClick={handleNextImage}>
-            <div className='bg-black opacity-50 w-5 md:w-16 flex text-center items-center'>
-              <BsChevronRight />
-            </div>
-          </Link>
         </div>
       </div>
   );
