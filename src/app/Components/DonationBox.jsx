25d0a7c0ded20156d5e1c4e494f75519ac85d78f
shahrazmirza <shahrazahmedmirza@live.com>
2024-03-20 13:05:41 +1100

    Commit

--- src/app/Components/DonationBox.jsx
+++ src/app/Components/DonationBox.jsx
@@ -1,7 +1,7 @@
 'use client'
 import { Container } from "@radix-ui/themes";
 import React, { useState } from "react";
-import { Button} from "@nextui-org/react";
+import { Button, RadioGroup, Radio, Input } from "@nextui-org/react";
 
 export default function DonationBox() {
   
@@ -12,36 +12,62 @@ export default function DonationBox() {
     const formattedValue = inputValue.replace(/\D/g, '').replace(/\B(?=(\d{3})+(?!\d))/g, ',');
     setAmount(formattedValue);
   };
+
+  const sizes = ["sm", "md", "lg"];
     
   return (
     <Container>
-      <div className="flex flex-col w-screen md:w-fit gap-5 text-white bg-black rounded-lg p-5 z-20">
+      <div className="flex flex-col w-screen md:w-fit gap-5 text-black bg-white rounded-lg p-5 z-20">
         <h1 className="flex justify-center text-3xl">Donate Now</h1>
-        <div className="grid grid-cols-2 m-2 rounded-lg bg-yellow-300 text-gray-800">
-          <Button className='text-lg font-bold p-2 border-r border-gray-400' type="radio">SADAQA</Button>
-          <Button className='text-lg font-bold p-2 border-gray-400' type="radio">ZAKAT</Button>
+        
+        <RadioGroup
+          label="Select Donation Type"
+          orientation="horizontal"
+        >
+          <Radio value="sadaqa" size="lg">SADAQA</Radio>
+          <Radio value="zakat" size="lg">ZAKAT</Radio>
+        </RadioGroup>
+        
+        {/* <div className="grid grid-cols-2 m-2 gap-5">
+          <Button className='text-lg font-bold p-2' type="radio">SADAQA</Button>
+          <Button className='text-lg font-bold p-2' type="radio">ZAKAT</Button>
+        </div> */}
+
+        <RadioGroup
+          label="Select Amount"
+          orientation="horizontal"
+        >
+          <Radio value="50" >$50</Radio>
+          <Radio value="100">$100</Radio>
+          <Radio value="250">$250</Radio>
+          <Radio value="500">$500</Radio>
+          <Radio value="1000">$1,000</Radio>
+          <div className="w-32">
+          <Input type="text" label="Custom Amount" placeholder="$"value={amount} onChange={handleInputChange}/>
         </div>
-        <p className="text-xl px-2">Select Amount</p>
-        <div className="flex flex-col md:flex-row rounded-lg border bg-yellow-300 m-2 ">
-          <div className="grid grid-cols-5 border-b border-gray-400 md:border-b-0">
-            <Button className='flex justify-center text-lg font-bold py-2 border-r border-gray-400 text-gray-800 px-4' type="radio">$50</Button>
-            <Button className='flex justify-center text-lg font-bold py-2 border-r border-gray-400 text-gray-800' type="radio">$100</Button>
-            <Button className='flex justify-center text-lg font-bold py-2 border-r border-gray-400 text-gray-800' type="radio">$250</Button>
-            <Button className='flex justify-center text-lg font-bold py-2 border-r border-gray-400 text-gray-800' type="radio">$500</Button>
-            <Button className='flex justify-center text-lg font-bold py-2 border-r-0 md:border-r border-gray-400 text-gray-800' type="radio">$1000</Button>
+        </RadioGroup>
+
+        {/* <p className="text-xl px-2">Select Amount</p>
+        <div className="flex flex-col md:flex-row m-2 ">
+          <div className="grid grid-cols-5 gap-2">
+            <Button className='flex justify-center text-lg font-bold py-2' color="warning" type="radio">$50</Button>
+            <Button className='flex justify-center text-lg font-bold py-2' color="warning" type="radio">$100</Button>
+            <Button className='flex justify-center text-lg font-bold py-2' color="warning" type="radio">$250</Button>
+            <Button className='flex justify-center text-lg font-bold py-2' color="warning" type="radio">$500</Button>
+            <Button className='flex justify-center text-lg font-bold py-2' color="warning" type="radio">$1000</Button>
           </div>
-          <div className="flex justify-center items-center border-b rounded md:border-b-0 bg-white text-gray-800">
+          <div className="flex justify-center items-center">
             <p className="px-2">$</p>
             <input
-              className='text-md font-bold border-none focus:outline-none py-2 pl-2'
+              className='text-md font-bold py-2 pl-2 w-36'
               type="text"
               value={amount}
               onChange={handleInputChange}
               placeholder="Custom Amount"
             />
           </div>
-        </div >
-        <Button className="m-2 rounded-lg  bg-green-700 text-white py-2 text-xl">Donate</Button>
+        </div > */}
+        <Button className="my-2 rounded-lg  bg-green-700 text-white py-2 text-xl">Donate</Button>
       </div>
     </Container>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,12 @@
-import type { Config } from "tailwindcss";
+import {nextui} from "@nextui-org/react";
 
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
@@ -13,8 +15,12 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontFamily: {
+        heading1: ['Herr Von Muellerhoff', 'cursive'],
+        heading2: ['Source Sans', 'sans-serif'],
+      },
     },
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [nextui()]
 };
-export default config;
