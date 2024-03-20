ad8f6b73b4f64275b02aa5f33f06e2e81f756493
shahrazmirza <shahrazahmedmirza@live.com>
2024-03-20 17:06:30 +1100

    Commit

--- src/pages/api/checkout_sessions.js
+++ src/pages/api/checkout_sessions.js
@@ -18,4 +18,41 @@ export async function checkout({lineItems}){
 		successUrl: `${window.location.origin}?session_id={CHECKOUT_SESSION_ID}`,
 		cancelUrl: window.location.origin
 	})
+
 }
+
+// const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+// export default async function handler(req, res) {
+//   if (req.method === 'POST') {
+
+//     const items = req.body.cartItem
+
+//         const transformedItems = items.map((item) => ({
+//             price_data: {
+//                 currency: "aud",
+//                 product_data: {
+//                   name: `${item.name.toUpperCase()} - ${item.size}`,
+//                 },
+//                 unit_amount: item.price * 100,
+//             },
+//             quantity: item.quantity,
+//         }))
+
+//     try {
+//       const session = await stripe.checkout.sessions.create({
+//         line_items: transformedItems,
+//         mode: 'payment',
+//         success_url: `${req.headers.origin}/Success`,
+//         cancel_url: `${req.headers.origin}/Cancel`,
+//       });
+//       res.json({"sessionURL": session.url});
+//     } catch (err) {
+//       console.log(err);
+//       res.status(err.statusCode || 500).json(err.message);
+//     }
+//   } else {
+//     res.setHeader('Allow', 'POST');
+//     res.status(405).end('Method Not Allowed');
+//   }
+// }
