e13c936030ba22b7573145660951e3cc7d726909
shahrazmirza <shahrazahmedmirza@live.com>
2024-03-20 18:33:29 +1100

    Commit

--- src/app/Components/DonationBox.jsx
+++ src/app/Components/DonationBox.jsx
@@ -1,28 +1,35 @@
 'use client'
 import { Container } from "@radix-ui/themes";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Button, RadioGroup, Radio, Input } from "@nextui-org/react";
 import { checkout } from '../../pages/api/checkout_sessions'
+import data from '../Data/Products.json';
 
 export default function DonationBox() {
   
   const sizes = ["sm", "md", "lg"];
 
-  const [selectedAmount, setSelectedAmount] = useState("custom");
-  const [customAmount, setCustomAmount] = useState("");
-
   const handleCheckout = async () => {
-    let price = selectedAmount === "custom" ? customAmount : selectedAmount;
-
     await checkout({
       lineItems: [
         {
-          price: price,
+          price: "price_1OwH9LHPIvs440h63Rs6yYoc",
           quantity: 1
         }
       ]
     });
-  };  
+  };
+
+  const [items, setItems] = useState([]);
+  
+  useEffect(() => {
+    console.log('', data);
+    if (data && data.length > 0) {
+      setItems(data);
+    } else {
+      console.error('');
+    }
+  }, []);
 
   return (
     <Container>
@@ -45,30 +52,32 @@ export default function DonationBox() {
           orientation="horizontal"
           defaultValue="custom"
           isInvalid={""}
-          errorMessage={selectedAmount === "custom" && !customAmount ? "Please enter a custom amount or select from the options" : ""}
+          errorMessage={"" ? "Please select or enter a custom amount" : ""}
         >
-          <Radio name="amount" value="50" checked={selectedAmount === "price_1OwH9LHPIvs440h63Rs6yYoc"} onChange={() => setSelectedAmount("price_1OwH9LHPIvs440h63Rs6yYoc")}>$50</Radio>
-          <Radio name="amount" value="100" checked={selectedAmount === "100"} onChange={() => setSelectedAmount("100")}>$100</Radio>
-          <Radio name="amount" value="250" checked={selectedAmount === "250"} onChange={() => setSelectedAmount("250")}>$250</Radio>
-          <Radio name="amount" value="500" checked={selectedAmount === "500"} onChange={() => setSelectedAmount("500")}>$500</Radio>
-          <Radio name="amount" value="1000" checked={selectedAmount === "1000"} onChange={() => setSelectedAmount("1000")}>$1,000</Radio>
-          <Radio name="amount" value="custom" checked={selectedAmount === "custom"} onChange={() => setSelectedAmount("custom")}></Radio>
+          <ul className='flex gap-2 pt-5'>
+            {items.filter(item => item.type === 'sadaqa').map((item, index) => (
+              <li key={index}>
+                <Radio value={item.value}>${item.value}</Radio>
+              </li>
+            ))}
+          </ul>
+
+          <p className="pt-5 px-3">OR</p>
+
           <div className="w-32">
             <Input
-              name="customAmount"
               isClearable
               type="text"
               label="Custom Amount"
               variant=""
               placeholder="$"
-              value={customAmount}
-              onChange={(e) => setCustomAmount(e.target.value)}
-              onClear={() => setCustomAmount("")}
+              defaultValue=""
+              onClear={() => console.log("input cleared")}
               className="max-w-xs"
             />
           </div>
         </RadioGroup>
-
+        
         <Button 
           className="my-2 rounded-lg  bg-green-700 text-white py-2 text-xl"
           onClick={handleCheckout}>Donate</Button>
