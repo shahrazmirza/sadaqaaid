4efc10523af031897d19f36bbe73560ae1e95ded
shahrazmirza <shahrazahmedmirza@live.com>
2024-03-20 17:50:20 +1100

    Commit

--- src/app/Components/DonationBox.jsx
+++ src/app/Components/DonationBox.jsx
@@ -45,9 +45,9 @@ export default function DonationBox() {
           orientation="horizontal"
           defaultValue="custom"
           isInvalid={""}
-          errorMessage={selectedAmount === "custom" && !customAmount ? "Please enter a custom amount" : ""}
+          errorMessage={selectedAmount === "custom" && !customAmount ? "Please enter a custom amount or select from the options" : ""}
         >
-          <Radio name="amount" value="50" checked={selectedAmount === "50"} onChange={() => setSelectedAmount("50")}>$50</Radio>
+          <Radio name="amount" value="50" checked={selectedAmount === "price_1OwH9LHPIvs440h63Rs6yYoc"} onChange={() => setSelectedAmount("price_1OwH9LHPIvs440h63Rs6yYoc")}>$50</Radio>
           <Radio name="amount" value="100" checked={selectedAmount === "100"} onChange={() => setSelectedAmount("100")}>$100</Radio>
           <Radio name="amount" value="250" checked={selectedAmount === "250"} onChange={() => setSelectedAmount("250")}>$250</Radio>
           <Radio name="amount" value="500" checked={selectedAmount === "500"} onChange={() => setSelectedAmount("500")}>$500</Radio>
