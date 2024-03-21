222e2a84932e7959f396e62c5ff22bfc55c3c3cd
shahrazmirza <shahrazahmedmirza@live.com>
2024-03-22 10:36:19 +1100

    Commit

--- src/app/Components/Hero.jsx
+++ src/app/Components/Hero.jsx
@@ -4,11 +4,10 @@ import 'animate.css';
 import DonationBox from '../Components/DonationBox';
 import { Container } from '@radix-ui/themes';
 
-
 const Hero = () => {
   return (
     <div className='h-screen relative'>
-      <image>
+      <div>
         <div className='absolute inset-0 overflow-hidden'>
           <Image
             src='/assets/hero1.jpg'
@@ -28,10 +27,10 @@ const Hero = () => {
           />
         </div>
         <div className='absolute inset-0 overflow-hidden bg-black opacity-50'></div>
-      </image>
+      </div>
       <Container className='absolute top-0 right-0 left-0 bottom-0'>
         <div className='md:flex h-screen items-center md:gap-20 md:p-10'>
-          <h1 className='fontHeading text-2xl py-10 md:text-5xl text-white text-center md:text-start drop-shadow-2xl p-5 md:p-0 animate__animated animate__zoomIn animate__delay-1s'>Join our movement with Sadaqa Aid to end poverty and transform lives globally</h1>
+          <h1 className='fontHeading text-2xl py-10 md:text-5xl text-white text-center md:text-start drop-shadow-2xl p-5 md:p-0 animate__animated animate__fadeIn animate__delay-1s'>Join our movement with Sadaqa Aid to end poverty and transform lives globally</h1>
           <DonationBox />
         </div>
       </Container>
