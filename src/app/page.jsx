75d15494d3d3e34f9034d044eb155e29dfca1c3b
shahrazmirza <shahrazahmedmirza@live.com>
2024-03-21 14:15:42 +1100

    Commit

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,14 +3,16 @@ import Navbar from './Components/Navbar'
 import Scroll_to_top from './Components/Scroll_to_top';
 import Whatsapp from './Components/Whatsapp'
 import Hero from './Components/Hero'
+import Footer from './components/Footer';
 
 export default async function Home() {
   return (
-    <div className='w-screen h-screen overflow-hidden'>
+    <div className='w-screen overflow-hidden'>
       <Navbar />
       <Scroll_to_top />
       <Whatsapp />
       <Hero />
+      <Footer />
     </div>
   )
 }
